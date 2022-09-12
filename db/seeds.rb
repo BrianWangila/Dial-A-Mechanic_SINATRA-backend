efe8051c09dbeb1bfe2aa4b52807472c7d98e1ef
Brian Wangila <bwanjalah@gmail.com>
2022-09-12 21:23:52 +0300

    deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ mechanics = Mechanic.create([
       about: Faker::Lorem.sentence(word_count: rand(20..50)),
       image_url: "https://media.istockphoto.com/photos/happy-black-auto-mechanic-working-at-car-workshop-and-looking-at-picture-id1289143731?k=20&m=1289143731&s=612x612&w=0&h=12DezHaSI7XrRudm4Exd5aDyidgB0k9-worzo5u8viw=",
       mechanic_name: "Moses Okumu",
-      number: "+254723456789",
+      number: "+254789",
       email: "okumu@info.com"
     },
     {
@@ -22,7 +22,7 @@ mechanics = Mechanic.create([
       about: Faker::Lorem.sentence(word_count: rand(20..50)),
       image_url: "https://upload.wikimedia.org/wikipedia/commons/6/68/Auto_Mechanic.jpg",
       mechanic_name: "Melvin Mogaka",
-      number: "+254734567891",
+      number: "+2567891",
       email: "melvin@info.com"
     },
     {
@@ -33,7 +33,7 @@ mechanics = Mechanic.create([
       about: Faker::Lorem.sentence(word_count: rand(20..50)),
       image_url: "https://www.floridacareercollege.edu/wp-content/uploads/sites/4/2020/06/3-Reasons-Why-Being-a-Mechanic-Could-Be-An-Amazing-Career-Florida-Career-College.jpeg",
       mechanic_name: "Stephen Kariuki",
-      number: "+254756789123",
+      number: "+2547523",
       email: "stephen@info.com"
     },
     {
@@ -44,14 +44,14 @@ mechanics = Mechanic.create([
       about: Faker::Lorem.sentence(word_count: rand(20..50)),
       image_url: "https://www.bls.gov/ooh/images/3010.jpg",
       mechanic_name: "Mildred Nafula",
-      number: "+254745678912",
+      number: "+25474912",
       email: "mildred@info.com"
     },
     {
       image_url: "https://ded7t1cra1lh5.cloudfront.net/media/444425/8c4725e9f774107d6eabac5027d596415e8980b9/original/Service2.jpg?1548177569",
       category: "Break and Transmission",
       shop_name: "Ravvy Car Clinic",      
-      number: "724941205",
+      number: "72495",
       mechanic_name: "Brian Wangila",
       email: "brian@gmail.coo",
       about: Faker::Lorem.sentence(word_count: rand(20..50)),
@@ -62,7 +62,7 @@ mechanics = Mechanic.create([
       image_url: "https://ded7t1cra1lh5.cloudfront.net/media/444425/8c4725e9f774107d6eabac5027d596415e8980b9/original/Service2.jpg?1548177569",
       category: "Heavy Machinery",
       shop_name: "Ravvy Car Clinic",
-      number: "724941205",
+      number: "72495",
       mechanic_name: "Brian Wangila",
       email: "brian@gmail.coo",
       about: Faker::Lorem.sentence(word_count: rand(20..50)),
